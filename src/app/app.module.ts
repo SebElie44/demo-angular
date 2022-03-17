538cca871c3830560331adc9701c2bf380e97a32
selie2021 <sebastien.elie@hotmail.fr>
2022-03-17 17:00:53 +0100

    javascript - angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MessagesEcritsComponent } from './composants/messages-ecrits/messages-ecrits.component';
+import { NouveauMessageComponent } from './composants/nouveau-message/nouveau-message.component';
+import { MessagerieComponent } from './composants/messagerie/messagerie.component';
+import { PersonnesActivesComponent } from './composants/personnes-actives/personnes-actives.component';
+import {FormsModule} from "@angular/forms";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessagesEcritsComponent,
+    NouveauMessageComponent,
+    MessagerieComponent,
+    PersonnesActivesComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
